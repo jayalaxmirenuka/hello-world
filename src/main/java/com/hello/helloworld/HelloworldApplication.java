b0fe2ce81cc2c530367fc2b589bfd97110b9c05c
jayalaxmirenuka <jayalaxmirenuka57@gmail.com>
2024-07-26 17:22:59 +0530

    files

---
+++ src/main/java/com/hello/helloworld/HelloworldApplication.java
@@ -0,0 +1,23 @@
+package com.hello.helloworld;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@SpringBootApplication
+@RestController
+public class HelloworldApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(HelloworldApplication.class, args);
+	}
+
+	@GetMapping("/")
+    public String hello() {
+        return "Hello Renuka how are you, Welcome!";
+    }
+	
+	
+}
